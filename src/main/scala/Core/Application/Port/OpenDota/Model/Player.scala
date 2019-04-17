4e933b27597800735cfb49d86c243cd130e4401e
vishalinp <37312657+vishalinp@users.noreply.github.com>
2019-04-17 17:19:47 +0200

    added TODOs and priority list

--- src/main/scala/Core/Application/Port/OpenDota/Model/Player.scala
+++ src/main/scala/Core/Application/Port/OpenDota/Model/Player.scala
@@ -1,42 +1,45 @@
 package Core.Application.Port.OpenDota.Model
 
+// TODO - ancillary tables to contain keys to join back to base player table: match_id, player_slot
+// TODO - priority: ability_targets, ability_uses, damage_x, gold_t, kills_log, lane_pos (sorry!), lh_t, obs_log, purchase_log, runes, sen_log
 case class Player(
     match_id: Long,
     player_slot: Int,
-    ability_targets: Map[String, Map[String, Int]],
-    ability_upgrades_arr: List[Int],
-    ability_uses: Map[String, Int],
+    ability_targets: Map[String, Map[String, Int]], // TODO [ability(header), target(header2), count(data2)]
+    ability_upgrades_arr: List[Int], // TODO [level(array_index), ability_id(array_value)]
+    ability_uses: Map[String, Int], // TODO [ability(header), count(data)]
     account_id: Long,
     actions: Map[Int, Int],
-//    additional_units: Option[String], // TODO - MatchSample 6
+    //additional_units: Option[String],  TODO [columns=headers, values=data] 1 row per list element
     assists: Int,
     backpack_0: Int,
     backpack_1: Int,
     backpack_2: Int,
-    buyback_log: List[BuyBackLog],
+    buyback_log: List[BuyBackLog], // TODO [columns=headers, values=data] 1 row per list element
     camps_stacked: Int,
 
     // connection_log,
 
     creeps_stacked: Int,
-    damage: Map[String, Int],
-    damage_inflictor: Map[String, Int],
-    damage_inflictor_received: Map[String, Int],
-    damage_taken: Map[String, Int],
-    damage_targets: Map[String, Map[String, Int]],
+    damage: Map[String, Int], // TODO [unit(header), damage(data)]
+    damage_inflictor: Map[String, Int], // TODO [source(header), value(data)]
+    damage_inflictor_received: Map[String, Int], // TODO [source_ability(header), value(data)]
+    damage_taken: Map[String, Int], // TODO [source_unit(header), value(data)]
+    damage_targets: Map[String, Map[String, Int]], // TODO [source(header), target(header2), value(data)]
     deaths: Int,
     denies: Int,
     dn_t: List[Int],
     firstblood_claimed: Int,
     gold: Int,
     gold_per_min: Int,
-    gold_reasons: Map[Int, Double],
+    gold_reasons: Map[Int, Double], // TODO [source_id(header), value(data)]
     gold_spent: Int,
-    gold_t: List[Int],
+    gold_t: List[Int], // TODO [time(array_index), ability_id(array_value)]
 
     // End of by hand
     hero_damage: Int,
     hero_healing: Int,
+    // TODO hero_hits: Map[String, Int], [source(header), hits(data)]
     hero_id: Int,
     item_0: Int,
     item_1: Int,
@@ -44,10 +47,20 @@ case class Player(
     item_3: Int,
     item_4: Int,
     item_5: Int,
+    // TODO item_uses: Map[String, Int], [item(header), uses(data)]
+    // TODO kill_streaks: Map[Int, Int], [length(header), occurences(data)]
+    // TODO killed: Map[String, Int], [unit(header), value(data)]
+    // TODO killed_by: Map[String, Int], [unit(header), value(data)]
+    // TODO kills_log: List[Map[String, Int]], [columns=headers, values=data]
+    // TODO lane_pos: Not sure how to represent this one. This looks like x_pos[y_pos:time] to me. See if it's possible, otherwise ignore.
     kills: Int,
     last_hits: Int,
     leaver_status: Int,
     level: Int,
+    // TODO lh_t: List[Int], if this could be rolled up by distinct count it would be easier to store
+    // TODO multi_kills: [multiple(header), occurences(data)]
+    // TODO obs_log List[Map[String...]], [time, x, y]
+    // TODO purchase_log: List[Map[String, Int]], [item(key), time(time)]
     obs_placed: Int,
     party_id: Int,
     party_size: Int,
@@ -59,12 +72,15 @@ case class Player(
     repicked: Option[Boolean],
     roshans_killed: Int,
     rune_pickups: Int,
+    // TODO runes: Map[String, Int], [rune_id(header), count(data)]
+    // TODO sen_log List[Map[String...]], [time, x, y]
     sen_placed: Int,
     stuns: BigDecimal,
     teamfight_participation: BigDecimal,
     tower_damage: Int,
     towers_killed: Int,
     xp_per_min: Int,
+    // TODO xp_reasons: Map[String, Int], [source_id(header), value(data)]
     personaname: Option[String],
     name: Option[String],
     last_login: Option[String],
